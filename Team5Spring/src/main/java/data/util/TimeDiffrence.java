bf2bf79035a735318108db434c8ead6727797a31
KImkihong <08kihong21@naver.com>
2020-06-24 16:54:11 +0900

    시간차 추가
    시간차 추가

--- Team5Spring/src/main/java/data/util/TimeDiffrence.java
+++ Team5Spring/src/main/java/data/util/TimeDiffrence.java
@@ -10,8 +10,8 @@ public class TimeDiffrence {
 		public static final int SEC = 60;
 		public static final int MIN = 60;
 		public static final int HOUR = 24;
-		//public static final int DAY = 30;
-		//public static final int MONTH = 12;
+		public static final int DAY = 30;
+		public static final int MONTH = 12;
 	}
 
 
@@ -38,8 +38,10 @@ public class TimeDiffrence {
 			} else if ((diffTime /= TIME_MAXIMUM.MIN) < TIME_MAXIMUM.HOUR) {
 				// hour
 				msg = (diffTime) + "시간 전";
+			} else if ((diffTime /= TIME_MAXIMUM.HOUR) < TIME_MAXIMUM.DAY ||(diffTime /= TIME_MAXIMUM.HOUR) < TIME_MAXIMUM.DAY){
+				msg = date.substring(0, 4)+"월 "+date.substring(8, 10)+"일";
 			} else {
-				msg = date.substring(0, 10);
+				msg = date.substring(5, 7)+"년 "+date.substring(5, 7)+"월 "+date.substring(8, 10)+"일";
 			}
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
